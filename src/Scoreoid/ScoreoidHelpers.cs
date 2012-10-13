fd7e7e68a1236d75a9478153e5b5d3785616092c
Rudi Grobler <rudi@bbd.co.za>
2012-10-13 07:23:50 +0200

    Add sanity checks

--- src/Scoreoid/ScoreoidHelpers.cs
+++ src/Scoreoid/ScoreoidHelpers.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Reflection;
 
 namespace Scoreoid
@@ -7,6 +8,11 @@ namespace Scoreoid
     {
         public static void Inject<T>(this Dictionary<string, string> p, T o)
         {
+            if (p == null)
+                throw new ArgumentNullException("p", "p can not be null.");
+            if (o == null)
+                throw new ArgumentNullException("o", "o can not be null.");
+
             IEnumerable<PropertyInfo> properties = o.GetType().GetRuntimeProperties();
             foreach (PropertyInfo property in properties)
             {
@@ -19,6 +25,11 @@ namespace Scoreoid
 
         public static string TrimResponse(this string str, string expectedResponse)
         {
+            if (string.IsNullOrEmpty(str))
+                throw new ArgumentNullException("str", "str can not be null or empty.");
+            if (string.IsNullOrEmpty(expectedResponse))
+                throw new ArgumentNullException("expectedResponse", "expectedResponse can not be null or empty.");
+
             int startTagLength = expectedResponse.Length + 2;
             int endTagLength = expectedResponse.Length + 3;
             return
