d5fe5f237e02d3086e6083a23d018c962eeb047e
Rudi Grobler <rudi@bbd.co.za>
2012-10-13 07:23:13 +0200

    Update AssemblyInfo + Add GlobalSuppressions

--- src/ScoreoidUI/Properties/AssemblyInfo.cs
+++ src/ScoreoidUI/Properties/AssemblyInfo.cs
@@ -1,14 +1,11 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("ScoreoidUI")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ScoreoidUI")]
-[assembly: AssemblyCopyright("Copyright © Rudi Grobler 2012")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyCompany("Rudi Grobler")]
+[assembly: AssemblyProduct("Scoreoid for Windows 8")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: ComVisible(false)]
+[assembly: CLSCompliant(true)]
